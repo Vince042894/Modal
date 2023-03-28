b7d3055b17691b7f9df55eb1b63a9d3676952f2e
runel042894 <85103751+runel042894@users.noreply.github.com>
2023-03-29 01:20:20 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [show, setShow] = useState(false); //Modal
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [isCancelled, setIsCancelled] = useState(false);
+  const [status, setStatus] = useState(true);
+
+  // function handle cancelation
+
+  function handleChange() {
+    setIsCancelled(!isCancelled);
+    setTimeout(() => {
+      setShow(false);
+    }, 2000);
+    setStatus(!status);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+        <h1>Your Subscriptions</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Subscription</th>
+              <th>Next Rebill</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>$49.99 per month</td>
+              <td>December 13, 2020</td>
+              <td>
+                <Button variant="primary" onClick={handleShow}>
+                  {status ? "Cancel" : "Cancelled"}
+                </Button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title style={{ color: "black" }}>Warning</Modal.Title>
+        </Modal.Header>
+        <Modal.Body style={{ color: "black" }}>
+          {isCancelled
+            ? "Are you sure you want to cancel your subscription?"
+            : "Subscription successfully cancelled"}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            No
+          </Button>
+          <Button variant="primary" onClick={handleChange}>
+            Yes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
-}
+};
 
 export default App;
